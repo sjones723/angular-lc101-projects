db0ceebfa10ea3d4eb0e0bb223f87437b53dfef4
Sydney Jones <sydneyjones87@gmail.com>
2022-12-08 11:34:25 -0600

    prt1&2

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Some stuff I saw that one time';
+  image1 = 'https://images.vice.com/motherboard/content-images/article/12266/1400183979256723.jpg';
+  image2 = 'https://newsbtc.com/wp-content/uploads/2022/11/doge-to-the-moon-scaled.jpeg';
+  image3 = 'https://www.kindpng.com/picc/m/128-1283992_transparent-doge-png-doge-png-png-download.png';
 
   constructor() { }
 
