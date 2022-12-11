e8d96d09aa05eadb5c6ac2c39ad63450ecf4ea07
Sydney Jones <sydneyjones87@gmail.com>
2022-12-10 20:18:37 -0600

    mvc

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -25,7 +25,10 @@ export class CandidatesComponent implements OnInit {
 
   // Code the addToCrew function here:
 
-
+addToCrew(person:object){
+if(!this.crew.includes(person)){
+  this.crew.push(person)}
+}
   // BONUS: Code the changeMissionName function here:
 
 
